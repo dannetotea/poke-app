9ae42ecb428e8af3ad402b145bb3b6cdadb95fda
Dan Netotea <dannetotea@gmail.com>
2020-06-14 12:19:59 +0300

    Install axios; fetch data from API; install react-router-dom; apply fixed background-image;

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const fetchData = () => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/ditto")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return <div className="App">{fetchData()}</div>;
 }
 
 export default App;
